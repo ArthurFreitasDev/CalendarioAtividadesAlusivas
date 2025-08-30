f24564712b922d663b740fbf41c382d7b2ac60e0
Arthu <Arthu@DESKTOP-8NDCHEJ>
2025-08-30 13:40:57 -0300

    Adicionar arquivos de projeto.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CalendarioAtividadesAlusivas")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CalendarioAtividadesAlusivas.Pages")]
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace CalendarioAtividadesAlusivas
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
