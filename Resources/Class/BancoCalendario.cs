23b7021d9fd5938d6f135a3e2410a92c5e1722ed
Arthur <arthurcesardefreitas@gmail.com>
2025-09-10 21:43:29 -0300

    commit

---
+++ Resources/Class/BancoCalendario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarioAtividadesAlusivas.Resources.Class
+{
+    internal class BancoCalendario
+    {
+        public int ID { get; set; }
+        public string Dia { get; set; }
+        public string NomeData { get; set; }
+    }
+}
