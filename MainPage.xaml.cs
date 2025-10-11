fa6fc20f5643263d90ebd5ac763c09a8bf4b783c
Arthur Cesar de Freitas <Arthurfreitas2006@hotmail.com>
2025-10-11 18:41:08 -0300

    commit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,26 @@
-﻿namespace CalendarioAtividadesAlusivas
+﻿using System.Text.Json;
+using CalendarioAtividadesAlusivas.Models;
+using static CalendarioAtividadesAlusivas.Models.Dias;
+namespace CalendarioAtividadesAlusivas
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-            
+            CarregarMeses();
+        }
+
+        private async void CarregarMeses()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("BancoDias.json");
+            using var reader = new StreamReader(stream);
+            string content = await reader.ReadToEndAsync();
+
+            var calendario = JsonSerializer.Deserialize<Lista_Janeiro>(content);
+
+            var primeiroDia = calendario.ListaJaneiro.Janeiro.First();
+            await DisplayAlert("Primeiro Dia", $"Dia: {primeiroDia.Dia}, Dia da Semana: {primeiroDia.Dia_Semana}, Data Especial: {primeiroDia.Data_Especial}", "OK");
         }
     }
 }
---
+++ Models/Dias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarioAtividadesAlusivas.Models
+{
+    public class Dias
+    {
+        public int Dia { get; set; }
+        public string Dia_Semana { get; set; }
+        public string Data_Especial { get; set; }
+
+        public class ListaJaneiro
+        {
+            public List<Dias> Janeiro { get; set; }
+        }
+        public class Lista_Janeiro
+        {
+            public ListaJaneiro ListaJaneiro { get; set; }
+        }
+    }
+}
